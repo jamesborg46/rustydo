527b654c6dd11d7cc6a1c0eb81b156a1b2e7965a
Indoril007 <jamesborg46@gmail.com>
2024-12-01 17:34:21 +0900

    Add date field to Todo struct

--- src/entities/todo.rs
+++ src/entities/todo.rs
@@ -1,17 +1,20 @@
+use chrono::NaiveDate;
 use std::fmt;
 
 pub struct Todo {
     id: u32,
     title: String,
     completed: bool,
+    date: NaiveDate,
 }
 
 impl Todo {
-    pub fn new(id: u32, title: String, completed: bool) -> Self {
+    pub fn new(id: u32, title: String, completed: bool, date: NaiveDate) -> Self {
         Todo {
             id,
             title,
             completed,
+            date,
         }
     }
 }
@@ -20,14 +23,15 @@ impl fmt::Display for Todo {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "Todo #{}: '{}' [{}]",
+            "Todo #{}: '{}' [{}] by {}",
             self.id,
             self.title,
             if self.completed {
                 "Completed"
             } else {
                 "Pending"
-            }
+            },
+            self.date
         )
     }
 }
@@ -38,9 +42,18 @@ mod tests {
 
     #[test]
     fn test_todo_creation() {
-        let todo = Todo::new(1, String::from("Test Todo"), false);
+        let todo = Todo::new(
+            1,
+            String::from("Test Todo"),
+            false,
+            NaiveDate::from_ymd_opt(2021, 1, 1).expect("Invalid date"),
+        );
         assert_eq!(todo.id, 1);
         assert_eq!(todo.title, "Test Todo");
         assert!(!todo.completed);
+        assert_eq!(
+            todo.date,
+            NaiveDate::from_ymd_opt(2021, 1, 1).expect("Invalid date")
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod entities;
 
+use chrono::NaiveDate;
 use entities::Todo;
 
 fn main() {
-    let todo = Todo::new(1, "Buy milk".to_string(), false);
+    let todo = Todo::new(
+        1,
+        "Buy milk".to_string(),
+        false,
+        NaiveDate::from_ymd_opt(2021, 1, 1).expect("Invalid date"),
+    );
     println!("Todo {}", todo);
 }
